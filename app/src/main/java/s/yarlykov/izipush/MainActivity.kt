0a023447e0ee8d5f297b99d483ffb0a043b814b1
ciscoff <ciscoff@ya.ru>
2021-05-20 21:57:56 +0300

    +

--- app/src/main/java/s/yarlykov/izipush/MainActivity.kt
+++ app/src/main/java/s/yarlykov/izipush/MainActivity.kt
@@ -3,6 +3,8 @@ package s.yarlykov.izipush
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.installations.FirebaseInstallations
 import com.google.firebase.messaging.FirebaseMessaging
 import s.yarlykov.pushsdk.utils.logIt
 
@@ -13,8 +15,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         showToken()
+        showAppId()
+    }
 
+    private fun showAppId() {
+        val task: Task<String> = FirebaseInstallations.getInstance().id
 
+        task.addOnSuccessListener { appId ->
+            logIt(" Firebase app installation: $appId")
+        }
     }
 
     private fun showToken() {
